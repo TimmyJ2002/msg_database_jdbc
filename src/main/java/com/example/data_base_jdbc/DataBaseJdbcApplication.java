4b46217f95b06179768ec5001b2a7711a9894c7a
timonjanosimsg <139860309+timonjanosimsg@users.noreply.github.com>
2023-07-26 09:44:14 +0300

    Initial commit

---
+++ src/main/java/com/example/data_base_jdbc/DataBaseJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.data_base_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBaseJdbcApplication.class, args);
+	}
+
+}
